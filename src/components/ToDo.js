2c9de47cf07946be953a721cb3f115eb64cadfab
Petar Nenov <petar_nenov@abv.bg>
2019-01-16 23:36:15 +0200

    ToDo class component to functional component !!! this

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,28 +1,47 @@
 import React from "react";
 import Info from "./Info";
 
-class ToDo extends React.Component {
-  constructor() {
-    super();
-  }
+// class ToDo extends React.Component {
+//   constructor() {
+//     super();
+//   }
 
-  render() {
-    //console.log(this.props)
-    return (
-      <div className="todos">
-        <h3>{this.props.resp.id}</h3>
+//   render() {
+//     //console.log(this.props)
+//     return (
+//       <div className="todos">
+//         <h3>{this.props.resp.id}</h3>
+//         <input
+//           type="checkbox"
+//           onChange={this.props.change}  
+//           checked={this.props.resp.checked}        
+//         />
+//         <p>
+//           {this.props.resp.price} - {this.props.resp.desc}
+//         </p>
+//         <Info counter={this.props.counter} />
+//       </div>
+//     );
+//   }
+// }
+
+const ToDo = (props) =>
+  {
+    return(
+    <div className="todos">
+        <h3>{props.resp.id}</h3>
         <input
           type="checkbox"
-          onChange={this.props.change}  
-          checked={this.props.resp.checked}        
+          onChange={props.change}  
+          checked={props.resp.checked}        
         />
         <p>
-          {this.props.resp.price} - {this.props.resp.desc}
+          {props.resp.price} - {props.resp.desc}
         </p>
-        <Info counter={this.props.counter} />
+        <Info counter={props.counter} />
       </div>
     );
   }
-}
+
 
 export default ToDo;
